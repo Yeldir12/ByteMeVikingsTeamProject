78cbffd4772ec6e1fc601e1d5ec1e67c18d0fad4
Ridley Furrows <RidleyFurrows@gmail.com>
2025-12-08 15:13:49 -0700

    Np login
    We dont have authentication working so now it wont require login yet.

--- routes/quest-board.js
+++ routes/quest-board.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-function requireLogin(req, res, next) {
-    if (!req.session.user) {
-        return res.redirect("/login");
-    }
-    next();
-}
-
-router.get('/', requireLogin, (req, res) => {
-    res.render('quest-board', {
-        user: req.session.user   // optional: send user to the page
-    });
+router.get('/', (req, res) => {
+  res.render('quest-board');
 });
 
 module.exports = router;
